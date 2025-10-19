3f7965a1f2d88bf71fe59be344dd7feae60e1f0b
Yannik Lehnhardt <yannik_lehnhardt@gmx.de>
2025-10-19 14:40:56 +0200

    add timer

--- test_dataset.py
+++ test_dataset.py
@@ -3,6 +3,7 @@ from torchsr.models import ninasr_b0, edsr_baseline, rdn_a, carn_m, rcan_g10r20f
 from torchvision.transforms.functional import to_pil_image, to_tensor
 import matplotlib.pyplot as plt
 import torch
+import time
 
 # Dataset laden
 dataset = Div2K(root="./scripts/data", scale=2, download=False)
@@ -24,6 +25,9 @@ match(str(inputModel).lower()):
 
 model.eval()
 
+start = time.time()
+print("Timer start")
+
 # Super-Resolution ausführen
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = model.to(device)
@@ -40,5 +44,8 @@ axs[1].imshow(sr)
 axs[1].set_title("Super-Resolved")
 axs[2].imshow(hr)
 axs[2].set_title("High-Resolution")
+
+end = time.time()
+print("Timer end ", end - start)
 for ax in axs: ax.axis("off")
 plt.show() # hr.show(), lr.show(), sr.show()
